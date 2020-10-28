6bcdd4092c4ffbac054ddb3430da40fc40ec0cd4
Leslie <leslie.appl@gmail.com>
2020-10-28 17:32:04 +0800

    Initial Commit

---
+++ Doc201028/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  Doc201028
+//
+//  Created by leslie on 10/28/20.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
